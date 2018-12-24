4e43ad0f00edba2a1b8b7dc4a8a29e9c312098e9
1z3r091 <1z3r091@gmail.com>
2018-12-25 02:29:30 +0900

    Add autoindent after new line

--- scripts/index.js
+++ scripts/index.js
@@ -52,10 +52,75 @@ force.on("tick", function() {
 });
 
 // EVENTS
+txtBox.addEventListener("keydown", function(e) {
+  if (e.which == 9) {
+    //ASCII tab
+    e.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+    var v = this.val();
+    if (start == end) {
+      $(this).val(v.slice(0, start) + "    " + v.slice(start));
+      this.selectionStart = start + 4;
+      this.selectionEnd = start + 4;
+      return;
+    }
+
+    var selectedLines = [];
+    var inSelection = false;
+    var lineNumber = 0;
+    for (var i = 0; i < v.length; i++) {
+      if (i == start) {
+        inSelection = true;
+        selectedLines.push(lineNumber);
+      }
+      if (i >= end) inSelection = false;
+
+      if (v[i] == "\n") {
+        lineNumber++;
+        if (inSelection) selectedLines.push(lineNumber);
+      }
+    }
+    var lines = v.split("\n");
+    for (var i = 0; i < selectedLines.length; i++) {
+      lines[selectedLines[i]] = "    " + lines[selectedLines[i]];
+    }
+
+    this.val(lines.join("\n"));
+  }
+});
+txtBox.addEventListener("keypress", function(e) {
+  if (e.which == 13) {
+    // ASCII newline
+    setTimeout(
+      function(that) {
+        var start = that.selectionStart;
+        var v = that.value;
+        var thisLine = "";
+        var indentation = 0;
+        for (var i = start - 2; i >= 0 && v[i] != "\n"; i--) {
+          thisLine = v[i] + thisLine;
+        }
+        for (var i = 0; i < thisLine.length && thisLine[i] == " "; i++) {
+          indentation++;
+        }
+        that.value =
+          v.slice(0, start) + " ".repeat(indentation) + v.slice(start);
+        that.selectionStart = start + indentation;
+        that.selectionEnd = start + indentation;
+      },
+      0.01,
+      this
+    );
+  }
+});
+
+//
 txtBox.addEventListener("keyup", function(e) {
   if (e.keyCode == 65 && e.ctrlKey) {
     e.target.select();
   }
+
   setSetting("documentContent", txtBox.value);
   removeAll();
   initialize();
@@ -106,9 +171,12 @@ function initialize() {
 
 function saveTxtLineToNode(txtBox, nodes) {
   var lines = txtBox.value.split("\n");
+
   lines.forEach(function(line, index) {
-    if (nodes[index]) nodes[index].label = line;
-    else {
+    //console.log(nodes[index]);
+    if (nodes[index]) {
+      nodes[index].label = line;
+    } else {
       let color = colors[Math.floor((line.search(/\S/) + 1) / 2)];
       let node = {
         label: line,
