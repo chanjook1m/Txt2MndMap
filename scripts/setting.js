dc8cae98e533b2cef91e7217842fba744ada25ac
1z3r091 <1z3r091@gmail.com>
2018-12-24 13:40:05 +0900

    Change default string

--- scripts/setting.js
+++ scripts/setting.js
@@ -1,6 +1,10 @@
 const map = { width: 960, height: 960 };
 const defaultStr =
-  "aasdfa sfasdfasdfad klfjasdfkjlas fjklasdjfkja sdkfjasdj fkjsdkf\nzzz";
+  "You cannot change the circumstances but you can change yourself. That is something you have charge of\n" +
+  "  Give thanks for a little and you will find a lot\n" +
+  "    Do what you can, with what you have, where you are\n" +
+  "      Don't think about what might go wrong, think about what could be right\n" +
+  "        The meaning of life is to find your gift. The purpose of life is to give it away";
 
 function getSetting(key) {
   let setting;
